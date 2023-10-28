c74d2008cfbd13ca0010d11a880306c1123ddca4
damiandamonte <damontedamian@gmail.com>
2023-10-27 21:07:13 -0300

    clase 6

---
+++ clase/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+const (
+	filename = "./data.csv"
+)
+
+func main() {
+	defer func() {
+		if err := recover(); err != nil {
+			// log.Println(err)
+			// os.Exit(1)
+			log.Fatal(err)
+		}
+	}()
+	readfile(filename)
+}
+
+func readfile(name string) {
+	file, err := os.ReadFile(name)
+	if err != nil {
+		panic(err)
+	}
+
+	data := strings.Split(string(file), ";")
+
+	var total float64
+
+	for i := 0; i < len(data)-1; i++ {
+		line := strings.Split(string(data[i]), ",")
+
+		// Omitir la cabecera
+		if i != 0 {
+			precio, err := strconv.ParseFloat(line[1], 64)
+			if err != nil {
+				log.Panicln("No se pudo convertir el precio")
+			}
+
+			cantidad, err := strconv.ParseFloat(line[2], 64)
+			if err != nil {
+				log.Panicln("No se pudo convertir la cantidad")
+			}
+
+			// Calculo el total del producto
+			totalProducto := precio * cantidad
+
+			// Su el total del producto al total de todos los productos
+			total += totalProducto
+		}
+
+		for i := 0; i < len(line); i++ {
+			fmt.Printf("%s\t\t", line[i])
+			if i == len(line)-1 {
+				fmt.Print("\n")
+			}
+		}
+	}
+
+	fmt.Printf("\t\t%s\t\t%.2f\n", "Total:", total)
+}
---
+++ mesas/main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+const (
+	filename = "./categorias.csv"
+)
+
+func main() {
+	defer func() {
+		if err := recover(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+	generarEstimacion(filename)
+}
+
+func generarEstimacion(name string) {
+	file, err := os.ReadFile(name)
+	if err != nil {
+		panic(err)
+	}
+
+	data := strings.Split(string(file), "\n")
+
+	// Mapa donde se totalizará cada categoria
+	estimaciones := map[string]float64{}
+
+	for i := 1; i < len(data)-1; i++ {
+		line := strings.Split(string(data[i]), ",")
+
+		// Si no existe la categoria en el mapa la creo
+		_, errb := estimaciones[line[1]]
+		if !errb {
+			estimaciones[line[1]] = 0.0
+		}
+
+		precioActual, err  := strconv.ParseFloat(line[4], 64)
+		if err != nil {
+			log.Panicln("No se pudo convertir el precioActual")
+		}
+		cantidadActual, err  := strconv.ParseFloat(strings.TrimSpace(line[5]), 64)
+		if err != nil {
+			log.Panicln("No se pudo convertir el cantidadActual")
+		}
+
+		// Calculo el total del producto y lo sumo a su categoria
+		estimaciones[line[1]] += precioActual * cantidadActual
+	}
+
+	// Creo al archivo donde se almacenarán las estimaciones y lo abro para poder escribirlo
+	fileOpen, err := os.OpenFile("estimaciones.csv", os.O_WRONLY|os.O_CREATE, 0644)
+	if err != nil {
+		panic(err)
+  }
+  defer fileOpen.Close()
+
+	// Agrego el titulo al archivo
+	_, err = fileOpen.WriteString("Categoría\t\tEstimativoPorCategoría\n")
+	if err != nil {
+		panic("Error al escribir el archivo")
+	}
+
+	// Agrego cada categoria y su total al archivo
+	for cat, total := range estimaciones {
+		_, err = fileOpen.WriteString(fmt.Sprintf("%s\t\t%.2f\n", cat, total))
+		if err != nil {
+			panic("Error al escribir el archivo")
+		}
+	}
+
+	//  Muestro por consola cada categoria y su total
+	fmt.Print("Categoría\t\tEstimativoPorCategoría\n")
+	for cat, total := range estimaciones {
+		fmt.Printf("%s\t\t%.2f\n", cat, total)
+	}
+}
